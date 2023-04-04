f5d0877cd0f4e65d6c952ba9a484e441dfde6155
Marcin Nowak-Liebiediew <marcin.liebiediew@dfinity.org>
2023-04-04 02:29:15 +0200

    reposition source files in filesystem

---
+++ extensions/sns/src/commands/mod.rs
@@ -0,0 +1,4 @@
+mod config;
+mod deploy;
+mod download;
+mod import;
--- extensions/sns/src/main.rs
+++ extensions/sns/src/main.rs
@@ -9,6 +9,14 @@ use crate::{
 
 use clap::Parser;
 
+//! Code for decentralizing dapps
+#![warn(clippy::missing_docs_in_private_items)]
+pub mod create_config;
+pub mod deploy;
+pub mod validate_config;
+
+/// The default location of an SNS configuration file.
+pub const CONFIG_FILE_NAME: &str = "sns.yml";
 mod config;
 mod deploy;
 mod download;

6442f184634c116d3b8a30643f6c778f8c2a9e9f
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2024-02-22 15:06:34 -0800

    test: prebuild extensions outside of tests (#90)

---
+++ .github/workflows/build-extension-manually.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+build_manually() (
+  local extension_name="$1"
+  package_version=$(cargo metadata --format-version=1 | jq -r '.workspace_members[]' | grep "$extension_name" | cut -d" " -f2)
+  cargo dist build --tag="$extension_name-v$package_version" # cargo-dist needs git tag only metadata-related stuff; it won't do git checkout, it will build from HEAD
+  extension_dir="$PREBUILT_EXTENSIONS_DIR/$extension_name"
+  arch_platform="$(get_arch_and_platform)"
+  mkdir -p "${extension_dir}"
+  tar xzf "target/distrib/$extension_name-$arch_platform.tar.gz" --strip-components 1 -C "$extension_dir"
+)
+
+get_arch_and_platform() {
+  ARCH=$(uname -m)
+  SYS=$(uname -s)
+
+  if [[ "$ARCH" == "x86_64" ]]; then
+    if [[ "$SYS" == "Darwin" ]]; then
+      echo "$ARCH-apple-darwin"
+    elif [[ "$SYS" == "Linux" ]]; then
+      echo "$ARCH-unknown-linux-gnu"
+    else
+      echo "System not recognized"
+    fi
+  elif [[ "$ARCH" == "arm64" && "$SYS" == "Darwin" ]]; then
+    echo "aarch64-apple-darwin"
+  else
+    echo "Architecture not recognized"
+  fi
+}
+
+build_manually "$1"
--- e2e/utils.sh
+++ e2e/utils.sh
@@ -1,6 +1,5 @@
 set -e
 
-export ORIG_HOME="$HOME"
 load "$GIT_ROOT_DIR"/e2e/bats-support/load
 load "$GIT_ROOT_DIR"/e2e/bats-assert/load
 
@@ -22,16 +21,10 @@ install_shared_asset() {
 }
 
 dfx_extension_install_manually() (
-    cd "$GIT_ROOT_DIR"
     local extension_name="$1"
-    package_version=$(HOME="$ORIG_HOME" cargo metadata --format-version=1 | jq -r '.workspace_members[]' | grep "$extension_name" | cut -d" " -f2)
-    HOME="$ORIG_HOME" cargo dist build --tag="$extension_name-v$package_version" # cargo-dist needs git tag only metadata-related stuff; it won't do git checkout, it will build from HEAD
     extensions_dir="$(dfx cache show)/extensions"
-    arch_platform="$(get_arch_and_platform)"
-    rm -rf "$extensions_dir/$extension_name-$arch_platform" "${extensions_dir:?}/$extension_name" # remove old versions
     mkdir -p "$extensions_dir"
-    tar xzf "target/distrib/$extension_name-$arch_platform.tar.gz" -C "$extensions_dir"
-    mv "$extensions_dir/$extension_name-$arch_platform" "$extensions_dir/$extension_name"
+    cp -R "$PREBUILT_EXTENSIONS_DIR/$extension_name" "$extensions_dir/$extension_name"
 )
 
 standard_setup() {
@@ -464,24 +457,5 @@ assert_no_dfx_start_or_replica_processes() {
     if [ -e .dfx/replica-configuration/replica-pid ];
     then
       ! ( kill -0 "$(< .dfx/replica-configuration/replica-pid)" 2>/dev/null )
-    fi
-}
-
-get_arch_and_platform() {
-    ARCH=$(uname -m)
-    SYS=$(uname -s)
-
-    if [[ "$ARCH" == "x86_64" ]]; then
-        if [[ "$SYS" == "Darwin" ]]; then
-            echo "$ARCH-apple-darwin"
-        elif [[ "$SYS" == "Linux" ]]; then
-            echo "$ARCH-unknown-linux-gnu"
-        else
-            echo "System not recognized"
-        fi
-    elif [[ "$ARCH" == "arm64" && "$SYS" == "Darwin" ]]; then
-        echo "aarch64-apple-darwin"
-    else
-        echo "Architecture not recognized"
     fi
 }

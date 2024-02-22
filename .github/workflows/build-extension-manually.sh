c7ee6ead7a175d2e6fbb6187e649e17e456cdcb9
Eric Swanson <eric.swanson@dfinity.org>
2024-02-22 14:08:06 -0800

    cleanup

--- .github/workflows/build-extension-manually.sh
+++ .github/workflows/build-extension-manually.sh
@@ -3,33 +3,32 @@
 set -e
 
 build_manually() (
-    local extension_name="$1"
-    package_version=$(cargo metadata --format-version=1 | jq -r '.workspace_members[]' | grep "$extension_name" | cut -d" " -f2)
-    cargo dist build --tag="$extension_name-v$package_version" # cargo-dist needs git tag only metadata-related stuff; it won't do git checkout, it will build from HEAD
-    extensions_dir="$PREBUILT_EXTENSIONS_DIR"
-    arch_platform="$(get_arch_and_platform)"
-    mkdir -p "${extensions_dir:?}/$extension_name"
-    tar xzf "target/distrib/$extension_name-$arch_platform.tar.gz" --strip-components 1 -C "$extensions_dir/$extension_name"
-    ls -lR "$PREBUILT_EXTENSIONS_DIR"
+  local extension_name="$1"
+  package_version=$(cargo metadata --format-version=1 | jq -r '.workspace_members[]' | grep "$extension_name" | cut -d" " -f2)
+  cargo dist build --tag="$extension_name-v$package_version" # cargo-dist needs git tag only metadata-related stuff; it won't do git checkout, it will build from HEAD
+  extensions_dir="$PREBUILT_EXTENSIONS_DIR"
+  arch_platform="$(get_arch_and_platform)"
+  mkdir -p "${extensions_dir:?}/$extension_name"
+  tar xzf "target/distrib/$extension_name-$arch_platform.tar.gz" --strip-components 1 -C "$extensions_dir/$extension_name"
 )
 
 get_arch_and_platform() {
-    ARCH=$(uname -m)
-    SYS=$(uname -s)
+  ARCH=$(uname -m)
+  SYS=$(uname -s)
 
-    if [[ "$ARCH" == "x86_64" ]]; then
-        if [[ "$SYS" == "Darwin" ]]; then
-            echo "$ARCH-apple-darwin"
-        elif [[ "$SYS" == "Linux" ]]; then
-            echo "$ARCH-unknown-linux-gnu"
-        else
-            echo "System not recognized"
-        fi
-    elif [[ "$ARCH" == "arm64" && "$SYS" == "Darwin" ]]; then
-        echo "aarch64-apple-darwin"
+  if [[ "$ARCH" == "x86_64" ]]; then
+    if [[ "$SYS" == "Darwin" ]]; then
+      echo "$ARCH-apple-darwin"
+    elif [[ "$SYS" == "Linux" ]]; then
+      echo "$ARCH-unknown-linux-gnu"
     else
-        echo "Architecture not recognized"
+      echo "System not recognized"
     fi
+  elif [[ "$ARCH" == "arm64" && "$SYS" == "Darwin" ]]; then
+    echo "aarch64-apple-darwin"
+  else
+    echo "Architecture not recognized"
+  fi
 }
 
 build_manually "$1"

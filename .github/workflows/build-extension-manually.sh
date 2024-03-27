9274776a21f14a1ae8b1592cd413f3aadd20465f
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2024-03-27 09:33:48 -0700

    fix: fail on any error in command pipeline (#97)

--- .github/workflows/build-extension-manually.sh
+++ .github/workflows/build-extension-manually.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -e -o pipefail
 
 build_manually() (
   local extension_name="$1"
   package_version=$(cargo metadata --format-version=1 | jq -r '.workspace_members[]' | grep "$extension_name" | cut -d" " -f2)
+  echo "package version for $extension_name: $package_version"
   cargo dist build --tag="$extension_name-v$package_version" # cargo-dist needs git tag only metadata-related stuff; it won't do git checkout, it will build from HEAD
   extension_dir="$PREBUILT_EXTENSIONS_DIR/$extension_name"
   arch_platform="$(get_arch_and_platform)"

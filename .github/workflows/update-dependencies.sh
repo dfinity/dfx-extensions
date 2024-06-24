ddc5de37c1df679792bf374c48e9f48ed494a062
Eric Swanson <eric.swanson@dfinity.org>
2024-06-24 15:41:00 -0700

    clean

--- .github/workflows/update-dependencies.sh
+++ .github/workflows/update-dependencies.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# Usage: ./update_dependencies.sh EXTENSION NEW_VERSION
+# Usage: .github/workflows/update-dependencies.sh EXTENSION NEW_VERSION
 # Run from project root
 
 EXTENSION=$1
 NEW_VERSION=$2
 
-# ensure both parameters provided
 if [ -z "$EXTENSION" ] || [ -z "$NEW_VERSION" ]; then
   echo "Usage: $0 EXTENSION NEW_VERSION"
   exit 1
@@ -15,12 +14,13 @@ fi
 EXTENSION_JSON="extensions/$EXTENSION/extension.json"
 DEPENDENCIES_JSON="extensions/$EXTENSION/dependencies.json"
 
+# Copy 'dependencies' field from extension.json to dependencies.json for the new version
 jq --slurpfile extension "$EXTENSION_JSON" --arg version "$NEW_VERSION" '
   .[$version] = (reduce ($extension[0].dependencies | to_entries[]) as $dep (
-    {}; .[$dep.key] = (if $dep.value | type == "string"
-                       then {"version": $dep.value}
-                       else $dep.value
+    {}; .[$dep.key] = (if $dep.value | type == "string" # normalize short-form version strings
+                       then {"version": $dep.value}     # to object with "version" key,
+                       else $dep.value                  # leave others as-is
                        end)
   ))
-  | {($version): .[$version]} + .
+  | {($version): .[$version]} + .                       # add new version as first entry
 ' "$DEPENDENCIES_JSON" | sponge "$DEPENDENCIES_JSON"

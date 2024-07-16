a385b06f29322af22ab92020da743d88d05a84c0
Martin Raszyk <martin.raszyk@dfinity.org>
2024-07-16 12:59:51 +0200

    use registry file instead of registry directory format

--- extensions/nns/src/install_nns.rs
+++ extensions/nns/src/install_nns.rs
@@ -107,9 +107,9 @@ pub async fn install_nns(
         nns_url: nns_url.to_string(),
         test_accounts,
         sns_subnets: Some(subnet_id.to_string()),
-        local_registry_store: network.local_server_descriptor.as_ref().map(|desc| {
+        local_registry_file: network.local_server_descriptor.as_ref().map(|desc| {
             desc.data_directory
-                .join("state/replicated_state/ic_registry_local_store")
+                .join("state/replicated_state/registry.proto")
         }),
     };
     ic_nns_init(&ic_nns_init_opts, dfx_cache_path).await?;
@@ -519,8 +519,8 @@ pub struct IcNnsInitOpts {
     /// A subnet for SNS canisters.
     /// Note: In this context we support at most one subnet.
     sns_subnets: Option<String>,
-    /// A directory storing the local registry store.
-    local_registry_store: Option<PathBuf>,
+    /// A file storing the registry content.
+    local_registry_file: Option<PathBuf>,
 }
 
 /// Calls the `ic-nns-init` executable.
@@ -538,9 +538,9 @@ pub async fn ic_nns_init(opts: &IcNnsInitOpts, dfx_cache_path: &Path) -> anyhow:
         "--wasm-dir".into(),
         opts.wasm_dir.as_os_str().into(),
     ];
-    if let Some(local_registry_store) = &opts.local_registry_store {
-        args.push("--registry-local-store-dir".into());
-        args.push(local_registry_store.into());
+    if let Some(local_registry_file) = &opts.local_registry_file {
+        args.push("--initial-registry".into());
+        args.push(local_registry_file.into());
     }
     for account in &opts.test_accounts {
         args.push("--initialize-ledger-with-test-accounts".into());
